b998e0910304f3560a47753f9769c16788fb969b
Simper007 <simperu@yandex.ru>
2019-04-09 17:51:05 +0500

    Третье домашнее задание

--- homework3/task1.py
+++ homework3/task1.py
@@ -4,3 +4,22 @@
 Затем создайте вторую функцию, удаляющую эти папки.
 Проверьте работу функций в этом же модуле.
 '''
+
+import os
+
+#print(os.getcwd())
+
+def create_dirs():
+    for i in range(1, 10):
+        os.mkdir(os.path.join(os.getcwd(),'dir_{}'.format(i)))
+        print('Создана папка:', os.path.join(os.getcwd(),'dir_{}'.format(i)))
+
+
+def del_dirs():
+    for i in range(1, 10):
+        os.rmdir(os.path.join(os.getcwd(),'dir_{}'.format(i)))
+        print('Папка', os.path.join(os.getcwd(), 'dir_{}'.format(i)), 'удалена')
+
+
+#create_dirs()
+#del_dirs()
