e53ca4aecf0a3770fa004ae60734396274cbe93d
Simper007 <simperu@yandex.ru>
2019-04-09 18:49:36 +0500

    Третье домашнее задание

--- homework3/task1.py
+++ homework3/task1.py
@@ -12,13 +12,15 @@ import os
 def create_dirs():
     for i in range(1, 10):
         os.mkdir(os.path.join(os.getcwd(),'dir_{}'.format(i)))
-        print('Создана папка:', os.path.join(os.getcwd(),'dir_{}'.format(i)))
+
+    return print('Созданы папки:', os.path.join(os.getcwd(),'dir_[1-9]'))
 
 
 def del_dirs():
     for i in range(1, 10):
         os.rmdir(os.path.join(os.getcwd(),'dir_{}'.format(i)))
-        print('Папка', os.path.join(os.getcwd(), 'dir_{}'.format(i)), 'удалена')
+
+    return print('Папки', os.path.join(os.getcwd(), 'dir_[1-9]'), 'удалены')
 
 
 #create_dirs()
